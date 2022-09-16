87f32f0bde21612acaa4a3c6aad6b6eb610cf476
Mikhail <47355726+Holdfast12@users.noreply.github.com>
2022-09-16 07:52:42 +0300

    123

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ cat /vagrant/access.log | grep -E -o '(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_
 
 stat -c %Z /vagrant/script.sh
 
-touch /vagrant/result
-#перевел все записи времени в секунды
-cat /vagrant/access.log | grep -E -o '[0-9]{,2}/[A-Z][a-z][a-z]/[0-9][0-9][0-9][0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]' | sed 's_/_ _g' | sed 's/:/ /' | while read -r line; do date -d "$line" "+%s"; done | while read -r line; do [[ $line -gt $(stat -c '%y' /vagrant/result | date "+%s") ]] && echo $line; done
+#создается файл, к котором содержатся временные отметки записей, занесенных после последнего запуска скрипта
+test -f /vagrant/timesample || touch /vagrant/timesample
+cat /vagrant/access.log | grep -E -o '[0-9]{,2}/[A-Z][a-z][a-z]/[0-9][0-9][0-9][0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]' | sed 's_/_ _g' | sed 's/:/ /' | while read -r line; do date -d "$line" "+%s"; done | while read -r line; do [[ $line -gt $(stat -c '%y' /vagrant/timesample | date "+%s") ]] && echo $line; done | while read -r line; do date --date @$line +"%d/%b/%Y:%H:%M:%S"; done
