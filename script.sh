b661bc1619d5df0a2206316d875d455c39ebeff6
Mikhail <47355726+Holdfast12@users.noreply.github.com>
2022-09-19 06:54:14 +0300

    123

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ cat /vagrant/access.log | grep -E -o '[0-9]{,2}/[A-Z][a-z][a-z]/[0-9][0-9][0-9][
 
 #Ошибки веб-сервера/приложения c момента последнего запуска
 echo -en "\n\nОшибки веб-сервера/приложения\n" >> /vagrant/result
-cat /vagrant/access.log | grep -E -o '[0-9]{,2}/[A-Z][a-z][a-z]/[0-9][0-9][0-9][0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]' | sed 's_/_ _g' | sed 's/:/ /' | while read -r line; do date -d "$line" "+%s"; done | while read -r line; do [[ $line -gt $t ]] && echo $line; done | while read -r line; do date --date @$line +"%d/%b/%Y:%H:%M:%S"; done | while read -r line; do grep $line /vagrant/access.log; done | grep -E -o 'HTTP/1.1" [1-5][0-9][0-9]' | grep -E -o 'HTTP/1.1" 400' | sort | uniq -c | sed 's:HTTP/1.1" 400:ошибок сервера с кодом 400 зафиксировано:' >> /vagrant/result
+cat /vagrant/access.log | grep -E -o '[0-9]{,2}/[A-Z][a-z][a-z]/[0-9][0-9][0-9][0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]' | sed 's_/_ _g' | sed 's/:/ /' | while read -r line; do date -d "$line" "+%s"; done | while read -r line; do [[ $line -gt $t ]] && echo $line; done | while read -r line; do date --date @$line +"%d/%b/%Y:%H:%M:%S"; done | while read -r line; do grep $line /vagrant/access.log; done | grep -E -o 'HTTP/1.1" [1-5][0-9][0-9]' | grep -E -o 'HTTP/1.1" 404' | sort | uniq -c | sed 's:HTTP/1.1" 404:ошибок сервера с кодом 404 зафиксировано:' >> /vagrant/result
 
 #Список всех кодов HTTP ответа с указанием их кол-ва с момента последнего запуска скрипта
 echo -en "\n\nкоды HTTP ответа\n" >> /vagrant/result
